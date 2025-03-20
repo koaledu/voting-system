cd2a05eac413eed7f997223089d9a587c5a5de83
Eduardo Monsalve <ed_ma.info@proton.me>
2025-03-20 13:27:21 -0500

    Add Nix flake for dependency management

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake for Bun";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "It's bunning time!"
+            exec fish
+          '';
+        };
+    };
+}
+
